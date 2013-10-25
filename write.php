2a230515d95ea04951ac92e1cf6013cc56552c9d
LincB <lincb123@gmail.com>
2013-10-25 15:32:09 -0400

    Various fixes

--- write.php
+++ write.php
@@ -1,20 +1,23 @@
 <?php
-//File not uploaded to Github for security
-include 'checkpass.php';
+include 'checkpass.php';//File not uploaded to Github for security
 $sqlpass = 'root';
 $sqluser = 'root';
 $sqldb = 'RiverWatch';
 $sqlhost = 'localhost';
 $allowed = checkpass($_GET['authcode']);
 if($allowed){
-    $rawdata = $_GET['data'];
+    $rawdata = $_GET['data']; //Delimit timestamp with periods (please)
     $data = explode(';',$rawdata);
     $con = mysqli_connect($sqlhost,$sqluser,$sqlpass,$sqldb);
     foreach($data as $dpoint){
         $points = explode(',',$dpoint);
-        if(!mysqli_query($con, 'SELECT * FROM river WHERE tstamp="'.$points[0].'"')){
-            mysqli_query($con,'INSERT INTO river VALUES ('.$points[0].','.$points[1].')');
-            echo("Written");
+        if(count(mysqli_fetch_array(mysqli_query($con, 'SELECT * FROM river WHERE date="'.$points[0].'"'),MYSQLI_NUM))===0){
+            if(!mysqli_query($con,'INSERT INTO river VALUES ('.$points[0].','.$points[1].')')){
+                echo(mysqli_error($con));
+            }
+            echo("Written ".$points[0].' '.$points[1].'<br/>');
+        }else{
+            echo("Write skipped<br/>");
         }
     }
     echo('Success');

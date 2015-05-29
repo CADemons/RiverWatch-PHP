65f10e9d527b21ed0819fbdeb8fe401d3341fece
LincB <LincB@users.noreply.github.com>
2015-05-29 19:20:30 -0400

    Resolved conflicts for cherry-pick of a1f42db

--- write.php
+++ write.php
@@ -1,27 +1,27 @@
 <?php
-require_once 'checkpass.php';//File not uploaded to Github for security
+require_once 'checkpass.php'; //File not uploaded to Github for security
 $sqluser = 'lberkley';
 //Password in chackpass file as $sqlpass
 $sqldb = 'riverdeepdata';
 $sqlhost = 'localhost';
 $allowed = checkpass($_GET['authcode']);
-if($allowed){
+if($allowed) {
     $rawdata = $_GET['data']; //Delimit timestamp with periods
-    $data = explode(';',$rawdata);
-    $con = mysqli_connect($sqlhost,$sqluser,$sqlpass,$sqldb);
-    foreach($data as $dpoint){
-        $points = explode(',',$dpoint);
-        if(count(mysqli_fetch_array(mysqli_query($con, 'SELECT * FROM river WHERE date="'.$points[0].'"'),MYSQLI_NUM))===0){
-            if(!mysqli_query($con,'INSERT INTO river VALUES ("'.$points[0].'","'.$points[1].'")')){
+    $data = explode(';', $rawdata);
+    $con = mysqli_connect($sqlhost, $sqluser, $sqlpass, $sqldb);
+    foreach($data as $dpoint) {
+        $points = explode(',', $dpoint);
+        if(count(mysqli_fetch_array(mysqli_query($con, 'SELECT * FROM river WHERE date="'.$points[0].'"'), MYSQLI_NUM)) === 0) {
+            if(!mysqli_query($con, 'INSERT INTO river VALUES ("'.$points[0].'","'.$points[1].'")')) {
                 echo(mysqli_error($con));
             }
             echo("Written ".$points[0].' '.$points[1].'<br/>');
-        }else{
+        } else {
             echo("Write skipped<br/>");
         }
     }
     echo('Success');
-}else{
+} else {
     header('HTTP/1.1 403 Forbidden');
     exit();
 }
